4267182063f72cffe00af7d118b9e7aabb5798f3
Harikrishna Bangaru <harikrishna.bangaru@ericsson.com>
2019-04-22 20:43:04 +0530

    added a change in ec2.tf

--- terraform-practice/ec2.tf
+++ terraform-practice/ec2.tf
@@ -1,4 +1,4 @@
-/*
+
 data "aws_ami" "latest-ubuntu" {
   most_recent = true
   executable_users = ["self"]
@@ -19,7 +19,7 @@ data "aws_ami" "latest-ubuntu" {
   }
 
 }
-*/
+
 resource "aws_key_pair" "test-key" {
   key_name   = "deployer-key"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCyn9FUgBglav/kKQosILzTTVYf86Uc+/XUfl0uwaZHwpbtFAb2PqBY0vZZHO0MQAifTth5+woGbkfhYNmsjAWE4Hs8T14DptFy+q9dGrZlgZVK1e3J0eu/4n85X9e4lc0vvFvvGIyOsOx++J99vElXkWPnWBYaSyy0xFjUyUVcYYEaYMr9vGhw7+OY4HZIV6hyafIrHAeFloOUdKHGd7PZ1iW1Mdgw3FKESOnWhL1uM8UFEKsaZ9yU3U6Ls6xkzmx38RSPeUJlJtPoK9nx3mKDWHCWCDNyrsqF+h3zhJaMzYc1k4gzEG4kYcxwYE3flxtygVx2euB9EwJ27B/ozz7wzJW4YMjntKdO88l9MiOhPjKU68Epe5MW7xo+ZmXNE88l6h9uygipOX4XAK5NHDCWOyym6HDJsJZVs3pLEh9suqenYS5MCR7RO4QG/0IKe57ydhRHIcF6a/t47Hh3VY14xNtkCjFqIrvtQKMTFTMajSyFa73UgpyeDt2aiH6IedYRGXHDo8jpFune1WsczTJeZBgZND2GC5gF+M9UwdADX7zEG4nsg36Wi3Z9PyRmc7S1OdDRDSLdVGtZaXu/aHaidZJXmAvIKXoMI75riAeusv+0PENMOLlhptVcNNVXEDGu0ndlNugTT468hMakQj06lVq4IraaXykPNvgFWryB+w== hkbangaru@gmail.com"
